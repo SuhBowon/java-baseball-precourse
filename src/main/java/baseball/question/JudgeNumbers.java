eeef40597ef1e488f32b4b3998a2eb2998241d56
sbwcartoon <sbwcartoon@gmail.com>
2022-10-02 02:00:03 +0900

    feat: 판정
    판정을 위한 비교 로직을 JudgeNumbers에 추가
    판정 결과를 저장하는 ScoreCount 추가
    JudgeNumbers 간 비교를 통해 strike, ball, out 개수를 판정하는 Judge 추가

--- src/main/java/baseball/question/JudgeNumbers.java
+++ src/main/java/baseball/question/JudgeNumbers.java
@@ -1,5 +1,7 @@
 package baseball.question;
 
+import java.util.Objects;
+
 public class JudgeNumbers {
     public final int first;
     public final int middle;
@@ -22,5 +24,28 @@ public class JudgeNumbers {
         int last = Integer.parseInt(judgeElements[2]);
 
         return new JudgeNumbers(first, middle, last);
+    }
+
+    public boolean has(int number) {
+        return number == this.first
+                || number == this.middle
+                || number == this.last;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return Objects.equals(this.toString(), obj.toString())
+                && getClass() == obj.getClass();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(this);
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName()
+                + String.format("[first=%s, middle=%s, last=%s]", first, middle, last);
     }
 }
